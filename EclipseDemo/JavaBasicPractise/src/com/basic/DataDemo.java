f4d33bf3c1d6644e43fbca1e97d093dab715b455
Lucky <231876009@qq.com>
2018-12-10 07:36:05 +0800

    upd

--- EclipseDemo/JavaBasicPractise/src/com/basic/DataDemo.java
+++ EclipseDemo/JavaBasicPractise/src/com/basic/DataDemo.java
@@ -6,6 +6,7 @@ public class DataDemo {
 	public static void main(String[] args) {
 		Date d = new Date();
 		System.out.println(d);
+		System.out.println(d.getTime());
 		d.setTime(1000 * 60 * 60 * 24 * 2);
 		System.out.println(d);
 		System.out.println(d.getTime());
