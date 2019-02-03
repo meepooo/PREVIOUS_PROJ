fcdf893483d07ab1afad2e863b24c4fd5ed4ea9e
Lucky <231876009@qq.com>
2019-02-04 02:04:08 +0800

    commit servlet

---
+++ EclipseDemo/HelloServlet/src/com/demo/servlet/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.demo.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet{
+
+//	@Override
+//	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		// TODO 自动生成的方法存根
+//		super.doGet(req, resp);
+//	}
+//
+//	@Override
+//	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		// TODO 自动生成的方法存根
+//		super.doPost(req, resp);
+//	}
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		System.out.println("hello servlet");
+	}
+	
+}
